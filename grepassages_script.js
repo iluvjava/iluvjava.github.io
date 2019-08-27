8885d128cb82597f1fb3a6d4712b6566b9ea4147
iluvjava <victor1301166040@gmail.com>
2019-08-27 10:51:32 -0700

    This is really cool

--- grepassages_script.js
+++ grepassages_script.js
@@ -5,14 +5,27 @@
     // JQuery : Classes Added to All Selected Entities. 
     const Settings1 = 
     {
-        "ul.list-group":["w-50", "p-3"]
+        "ul.list-group":["w-75", "p-3"], 
+        "div.card": ["w-100", "mx-auto","my-2", "align-items-center", "pt-4"],
+        "div.mydisplay": ["text-center", "bg-primary", "text-white"], 
+        "div.passage-entry":["container-fluid", "w-75"],
+        "h3": ["my-3"], 
+        "div.col": ["mx-0", "w-100"]
     };
+    //BootStrap Settings end.
     const ALLSETTINGS = 
     [
         Settings1
     ];
-    //BootStrap Settings end.
-
+    
+    //Listeners and their elements.
+    const ALLLISTENERS = 
+    {
+        "ul":()=>
+        {
+            console.log("Listener triggered on element: " + this);
+        }
+    }
 
     "use strict";
 
@@ -27,42 +40,47 @@
         prepareAllListeners();
     }
     
+    /**
+     * A function that reads from the ALLSETTINGS and apply all the setiings in it.
+     */
     async function applyAllSettings()
     {
         console.log("Applying All Settings... ");
         for (let i = 0; i < ALLSETTINGS.length; i++)
         {
             let setting = ALLSETTINGS[i];
-            for (
-                    let j = 0, val = Object.values(setting)[j], key = Object.keys(setting)[j];
-                    j < Object.keys(setting).length;
-                    val = Object.values(setting)[++j], 
-                    key = Object.keys(setting)[j]
-                )
+            for 
+            (
+                let j = 0, val = Object.values(setting)[j], key = Object.keys(setting)[j];
+                j < Object.keys(setting).length;
+                val = Object.values(setting)[++j], 
+                key = Object.keys(setting)[j]
+            )
             {
-                
                 console.log("Looking for element with css: " + key)
                 let targets = qs(key);
                 let settingclasseslist = val;
-                for (
+                for 
+                (
                     let k = 0, targetelement = targets[k]; 
                     k < targets.length;
-                    targetelement = targets[++k])
+                    targetelement = targets[++k]
+                )
                 {
                     console.log("Applying Settings to element: " + targetelement);
-                    for ( 
+                    for 
+                    ( 
                             let l = 0, classtoadd = settingclasseslist[l]; 
                             l < settingclasseslist.length; 
                             classtoadd = settingclasseslist[++l]
-                        )
-                        {
-                            console.log("add class: " + classtoadd);
-                            targetelement.classList.add(classtoadd);
-                        }
+                    )
+                    {
+                        console.log("add class: " + classtoadd);
+                        targetelement.classList.add(classtoadd);
+                    }
                 }
             }
         }
-
     } 
 
     /**
@@ -70,7 +88,6 @@
      */
     async function prepareAllListeners()
     {
-
         console.log("Preparing All Listeners.");
     }
 
