2ec860c97acd91ed2b1b852fd7bcbfb062160640
iluvjava <victor1301166040@gmail.com>
2019-08-29 19:50:22 -0700

    this is new stuff

--- README.md
+++ README.md
@@ -8,4 +8,37 @@ it's just a few html css files.
         - Add then passge title to the dropdown list as <li>
     - Hide unviewing passages on page
         - Make one of the passage entry to be on page by default. 
-        - Default unhidden must be manually coded in. 
+        - Default unhidden must be manually coded in.
+
+
+
+
+# About The Technical Details #
+## Promises and Async Function ##
+- [Link](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function)
+- An async function can make calls to functions that return promises and then await all of the to finish. 
+- await keyword can only be used for function that returns a promise. 
+- Function that returns a promise: 
+```
+    function returnAPromise()
+    {
+        return new promise
+        (
+            resolve =>
+            {
+                // what you wanna do here
+                resolve(//your result here.);
+            }
+        )
+    }
+```
+- The promise can return something, just use the resolve() fxn to return to 
+the the functions awaiting.
+- The let something = await somethingelse() will make the execute synchroous, should use promise.all()
+to execute shits all at the same time and wait for all of the them to finish. 
+
+## Placement of the script tags ##
+- Global module patters ensure functions members are not accessible after website has been set up. 
+- The main script of the page must be called before the boostrap. 
+    - This is the case because all the DOM objects must be set up properly before styling and 
+    adding listeners to them. 
--- grepassages_script.js
+++ grepassages_script.js
@@ -8,7 +8,7 @@
         "ul.list-group":["w-75", "p-3"], 
         "div.card": ["w-100", "mx-auto","my-2", "align-items-center", "pt-4"],
         "div.mydisplay": ["text-center", "bg-primary", "text-white"], 
-        "div.passage-entry":["container-fluid", "w-75"],
+        "div.passage-entry":["container-fluid", "w-100"],
         "h3": ["my-3"], 
         "div.col": ["mx-0", "w-90"],
         "nav":["navbar-expand-lg", "navbar-light", "bg-light", "sticky-top"], 
@@ -23,29 +23,62 @@
     //Listeners and their elements.
     const ALLLISTENERS = 
     {
-        "ul":()=>
-        {
-            console.log("Listener triggered on element: " + this);
-        }
+        "#my-dropdown-menu > *":
+        [
+            "click", 
+            (e)=>
+            {
+                console.log("Listener called on element: " +
+                 e.target.innerText);
+                let id = e.target.innerText.split(" ")[1];
+                let elements_to_hide = qs(".passage-entry *:not(.auto-hidden)");
+                for 
+                (  
+                    let i = 0, e = elements_to_hide[i];
+                    i < elements_to_hide.length;
+                    e = elements_to_hide[++i]
+                )
+                {
+                    e.classList.add("auto-hidden");
+                }
+
+                let elements_to_show = qs("#p" + id + " *");
+                for 
+                (  
+                    let i = 0, e = elements_to_show[i];
+                    i < elements_to_show.length;
+                    e = elements_to_show[++i]
+                )
+                {
+                    e.classList.remove("auto-hidden");
+                }
+            }
+        ], 
+
+        ".list-group-item": 
+        [
+            ["mouseover","mouseout" ],
+            (e)=>
+            {
+                console.log(e.type + "Event At: "+ e.target.innerText);
+            }
+        ]
     }
 
-    let DropdownManu = qs(".my-dropdown-menu");
-
     "use strict";
-
-    window.addEventListener("load", init);
 
     /**
      * Initialization when window is loaded. 
      */
     function init()
     {
-        applyAllSettings();
-        prepareAllListeners();
+        let p = setupTheWebsite();
+        p.then(prepareAllListeners());
     }
     
     /**
-     * A function that reads from the ALLSETTINGS and apply all the setiings in it.
+     * A function that reads from the ALLSETTINGS and apply all the 
+     * setiings in it.
      */
     function applyAllSettings()
     {
@@ -74,9 +107,9 @@
                     console.log("Applying Settings to element: " + targetelement);
                     for 
                     ( 
-                            let l = 0, classtoadd = settingclasseslist[l]; 
-                            l < settingclasseslist.length; 
-                            classtoadd = settingclasseslist[++l]
+                        let l = 0, classtoadd = settingclasseslist[l]; 
+                        l < settingclasseslist.length; 
+                        classtoadd = settingclasseslist[++l]
                     )
                     {
                         console.log("add class: " + classtoadd);
@@ -85,28 +118,115 @@
                 }
             }
         }
-    } 
+    }
 
     /**
-     * Using the const in the field to add all the listen to their desired elements .
+     * Sets up the website, all the visual elements will 
+     * be completely added after this function call. 
+     */
+    function setupTheWebsite()
+    {
+        return new Promise
+        (
+            resolve =>
+            {   
+                prepareDropDownMenu();
+                applyAllSettings();
+                resolve(null);
+            }
+        );
+    }
+
+    /**
+     * Using the const in the field to add all the listen to their 
+     * desired elements .
      */
     async function prepareAllListeners()
     {
         console.log("Preparing All Listeners.");
+        //debugger;
+        for(let i = 0; i < Object.keys(ALLLISTENERS).length; i++)
+        {
+            let k = Object.keys(ALLLISTENERS)[i];
+            let v = Object.values(ALLLISTENERS)[i];
+            let all_element_to_add = qs(k);
+           
+            for (let i = 0; i < all_element_to_add.length; i++)
+            {
+                addEventListenersTo(all_element_to_add[i], v[0], v[1]);
+                console.log("adding element to: " + all_element_to_add[i]);
+            }
+        }
     }
 
     /**
-     * Function scans the page for the passages and setup the dropdown manu for easy nevigation.
+     * Add a list of events with one handlers to a certain element.
+     * @param {Object} element 
+     * @param {Array String} eventlist 
+     * @param {Function} handlers 
+     */
+    function addEventListenersTo(element, eventlist, handlers)
+    {
+        if(typeof eventlist === "object")
+        {
+            eventlist.forEach
+            (
+                (e) =>
+                {
+                    element.addEventListener(e, handlers);
+                }
+            );
+        }
+        element.addEventListener(eventlist, handlers);
+    }
+
+    /**
+     * Function scans the page for the passages and setup the dropdown menu for
+     *  easy nevigation.
      * - It only creates the 
      * @param {Fxn} funccall
      * The function it should call after the asyn execution. 
      */
-    async function prepareDropDownManu(funccall = null)
+    function prepareDropDownMenu()
     {
-        if (funccall !== null)
-        {
-            funccall();
-        }
+        return new Promise
+        (
+            resolve =>
+            {
+                let passages = qs(".passage-entry");
+                for
+                (
+                    let i = 0;
+                    i < passages.length; 
+                    ++i
+                )
+                {
+                    let passagetitle = passages[i].children[0].innerText; 
+                    console.log(passagetitle);
+                    //debugger;
+                    qs("#my-dropdown-menu")[0].appendChild(
+                        createDropdownMenuItem(passagetitle));
+                }
+                resolve(null);
+            }
+        );
+    }
+
+    /**
+     * Creates dropdown menu item for bootstrap dropdown menu. 
+     * @param {string} text
+     * The text displayed on the droped down item.
+     */
+    function createDropdownMenuItem(text)
+    {
+        let newelement = document.createElement("div");
+        let newinnerelement = document.createElement("a");
+        newelement.classList.add("dropdown-menu");
+        newelement.setAttribute("aria-labelledby", "navbarDropdownMenuLink");
+        newinnerelement.classList.add("dropdown-item");
+        newinnerelement.innerText = text;
+        newelement.appendChild(newinnerelement);
+        return newinnerelement;
     }
 
     /**
@@ -127,6 +247,11 @@
         {
             throw new Error("Cannot find element: " + q);
         }
+        return res; 
     }
+
+    //Run everything in the script.
+    init();
+
 })
 ();
